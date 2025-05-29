933c4bfe0680dd71bf9398685f2af6f9f08d8148
maricielo <cielotapia198@gmail.com>
2025-05-28 23:06:28 -0500

    agrege dentro del codigo una introduccion que le da la bienbenida al usuario

--- ejerciciobucle1.cpp
+++ ejerciciobucle1.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main(){
     int n, M = 7;
-    cout << Ingrese la cantidad de multiplos de 7 que desea conocer: << endl;
+    cout << "bienvenido a mi programa" << endl;
+    cout <<"Ingrese la cantidad de multiplos de 7 que desea conocer:" << endl;
     cin >> n;
     
     for(int i = 1;i <= n;i++){
     	
-        cout << M << , ;
+        cout << M << "," ;
         M = M + 7;
     }
     cout << endl;
