bb9cd4665980ce94109f885bbec83470a258d7ab
maricielo <cielotapia198@gmail.com>
2025-05-29 23:31:08 -0500

    Agregando una bienvenida al usuario y agradecimiento por utilizar el programa, cambiando las variables por unas mas descriptivas, tambien mejorando el codigo llamando a una funcion que se encargue de encontrar al mayor numero y validando que la cantidad de numeros a evaluar sean mayores que cero

--- ejercicioNumeroMayor.cpp
+++ ejercicioNumeroMayor.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int obtenerNumeroMayor(int n){
+	int numero, mayor;
+    cout <<"Ingrese el primer numero:" << endl;
+    cin >> mayor;
+        
+    for( int i = 2; i <= n; i++) {
+        cout <<"Ingrese el siguiente numero:"<< endl;
+        cin >> numero;
+        if(numero > mayor) {
+            mayor = numero;
+        }
+    }
+    return mayor;
+
+}
+
+
 int main() {
-    int n, num;
-    double NM;
-    
-    cout << Cantidad de numeros: << endl;
+    int n, num, mayor;
+    cout << "BIENVENIDO A MI PROGRAMA!" << endl;
+    cout <<"Ingrese la cantidad de numeros que desea evaluar: "<< endl;
     cin >> n;
     
     if(n > 0) {
-        cout << Ingrese el primer numero: << endl;
-        cin >> NM;
-        
-        for( int i = 1; i < n; i++) {
-            cout << Ingrese el siguiente numero: << endl;
-            cin >> num;
-            if(num > NM) {
-                NM = num;
-            }
-        }
-        cout << El mayor numero es:  << NM << endl;
+        int mayor = obtenerNumeroMayor(n);
+        cout <<"El mayor numero es: "<< mayor << endl;
+	} else {
+		cout << "ingrese una cantidad mayor que cero porfavor." << endl;
 	}
-
+	cout << "GRACIAS POR UTILIZAR MI PROGRAMA <3" << endl;
     return 0;
 }
