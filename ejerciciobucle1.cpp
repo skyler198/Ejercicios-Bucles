3b84c32abb3f0c8bfe4f591ea14635d2e683b564
maricielo <cielotapia198@gmail.com>
2025-05-30 22:53:57 -0500

    agregando una bienvenida y agradecimiento al usuario por usar mi programa, mejorando el proceso para obtener los multiplos de 7, eliminando la variable M porque se volvio innecesaria, utilizando la condicional if para colocar comas y que el resultado se vea mas limpio

--- ejerciciobucle1.cpp
+++ ejerciciobucle1.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 int main(){
-    int n, M = 7;
-    cout << "bienvenido a mi programa" << endl;
+    int n;
+    cout << "BIENVENIDO A MI PROGRAMA!" << endl;
     cout <<"Ingrese la cantidad de multiplos de 7 que desea conocer:" << endl;
     cin >> n;
     
-    for(int i = 1;i <= n;i++){
+    cout << "Los primeros " << n << " multiplos de 7 son:" << endl;
+    
+    for(int i = 1; i <= n; i++){
     	
-        cout << M << "," ;
-        M = M + 7;
+        cout << ( i * 7); 
+		if ( i < n){
+			cout << ", ";
+		}
     }
     cout << endl;
+    cout << "GRACIAS POR USAR MI PROGRAMA <3";
     return 0;
 }
